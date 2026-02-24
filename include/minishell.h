55a731c721d06555a399bc5718afd166b0bebe25
Juan Cunha <juan.gucunha@gmail.com>
2026-02-24 13:33:35 +0100

    remove unused colors

--- include/minishell.h
+++ include/minishell.h
@@ -39,27 +39,7 @@
 
 # include "libft.h"
 
-/* ============================== COLORS ================================ */
-
-# define COLOR_GOLD     "\x1b[38;5;220m"
-
-# define COLOR_BLACK    "\x1b[30m"
-
-# define COLOR_RED      "\x1b[31m"
-
-# define COLOR_GREEN    "\x1b[32m"
-
-# define COLOR_YELLOW   "\x1b[33m"
-
-# define COLOR_BLUE     "\x1b[34m"
-
-# define COLOR_MAGENTA  "\x1b[35m"
-
-# define COLOR_CYAN     "\x1b[36m"
-
-# define COLOR_WHITE    "\x1b[37m"
-
-# define COLOR_RESET    "\x1b[0m"
+/* ============================== BANNER ================================ */
 
 #ifndef MINISHELL_BANNER
 # define MINISHELL_BANNER ""
